7d6d6ba488a0188f147a0c16f867c7b9bcd64e46
tkhs-dev <tokuhausu@gmail.com>
2021-03-12 16:54:46 +0900

    プロジェクトを作成

---
+++ MyDNS IP Notice Tool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyDNS_IP_Notice_Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
